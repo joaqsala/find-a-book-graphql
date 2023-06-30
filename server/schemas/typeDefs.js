a07d4382087fc3ea3a2860347725a3ba73d40577
Joaquin Salazar <joaqsala@gmail.com>
2023-06-30 13:36:45 -0500

    updated main server file to include ApolloServer

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,7 +23,17 @@ const typeDefs = gql`
     type Auth {
         token: ID!
         user: User
-  }
+    }
+
+    input NewBook {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
 
     type Query {
         user(_id: String): [User]
@@ -32,7 +42,8 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         createUser(_id: String!, username: String!, email: String!, password: String!): [User]
-        saveBook(_id: String!, bookId: String!): [User]
+        saveBook(bookData: NewBook!): [User]
         deleteBook(_id: String!, bookId: String!): [User]
     }
 `
+//ask about bookData ??
